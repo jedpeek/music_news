32bad29939aa99263edb9d43ad9f9d6d580d53b3
Jed Peek <jedpeek@yahoo.com>
2018-06-28 18:02:23 +0000

    added all class variable and save methods to stories

--- music_news/lib/music_news/scraper.rb
+++ music_news/lib/music_news/scraper.rb
@@ -1,2 +1,3 @@
 class MusicNews::Scraper
+  
 end
--- music_news/lib/music_news/stories.rb
+++ music_news/lib/music_news/stories.rb
@@ -1,23 +1,28 @@
 
 class MusicNews::Stories
   attr_accessor :headline, :author, :url, :synopsis
+@@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
 
     def self.scrape_stories
-      stories = []
       doc = Nokogiri::HTML(open("https://www.npr.org/sections/music-news/"))
       i = 0
 
       while i < 10
       story = self.new
-
       story.headline = doc.css(".item .item-info h2.title")[i].text.strip #headline
-      #story.author = doc.css("ol").css(".story-meta .byline")[i].text.strip.gsub("By ", '') #author
       story.url = doc.css(".item .item-info .teaser a")[i]['href'] #url to article
       story.synopsis = doc.css(".item .item-info .teaser")[i].text.strip #summary/synopsis
-
-      stories << story
+      story.save
       i += 1
         end
-      stories
+      @@all
     end
 end
